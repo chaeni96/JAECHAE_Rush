957dc6beabf1970a18614c4e1b9684a1e5eb0ec3
chaeni96 <cogus080520@gmail.com>
2022-06-11 13:47:35 +0900

    디폴트 수정

--- Default/Tool/ToolView.cpp
+++ Default/Tool/ToolView.cpp
@@ -65,21 +65,7 @@ void CToolView::OnInitialUpdate()
 		return;
 	}
 
-	//m_pSingle = new CSingleTexture;
-	//m_pSingle->InsertTexture(L"../Texture/Cube.png");
 
-
-	if (FAILED(CTextureMgr::Get_Instance()->InsertTexture(L"../Texture/Cube.png", TEX_SINGLE, L"Cube")))
-	{
-		AfxMessageBox(L"Cube Image Insert failed");
-		return;
-	}
-
-	if (FAILED(CTextureMgr::Get_Instance()->InsertTexture(L"../Texture/Stage/Terrain/Tile/Tile%d.png", TEX_MULTI, L"Terrain", L"Tile", 36)))
-	{
-		AfxMessageBox(L"Tile Image Insert failed");
-		return;
-	}
 
 }
 
@@ -93,43 +79,6 @@ void CToolView::OnDraw(CDC* /*pDC*/)
 		return;
 
 	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
-
-	//pDC->Rectangle(100, 100, 200, 200);
-
-
-	D3DXMATRIX		matWorld, matScale, matRotZ, matTrans;
-
-	D3DXMatrixIdentity(&matWorld);
-	D3DXMatrixScaling(&matScale, 1.f, 1.f, 1.f);
-	D3DXMatrixRotationZ(&matRotZ, D3DXToRadian(45.f));
-	D3DXMatrixTranslation(&matTrans, 400.f, 300.f, 0.f);
-
-	matWorld = matScale *  matTrans;
-
-	CDevice::Get_Instance()->Get_Sprite()->SetTransform(&matWorld);
-
-	/*float		fX = m_pSingle->Get_Texture()->tImgInfo.Width / 2.f;
-	float		fY = m_pSingle->Get_Texture()->tImgInfo.Height / 2.f;*/
-
-	//const TEXINFO*		pTexInfo = CTextureMgr::Get_Instance()->Get_Texture(L"Cube");
-
-	const TEXINFO*		pTexInfo = CTextureMgr::Get_Instance()->Get_Texture(L"Terrain", L"Tile", 3);
-	
-	if (nullptr == pTexInfo)
-		return;
-	
-	float		fX = pTexInfo->tImgInfo.Width / 2.f;
-	float		fY = pTexInfo->tImgInfo.Height / 2.f;
-
-	m_pDevice->Render_Begin();
-
-	m_pDevice->Get_Sprite()->Draw(pTexInfo->pTexture,	// 그리고자 하는 텍스처
-								nullptr, // 출력할 이미지 영역에 대한 rect 포인터, null인 경우 이미지의 0, 0 기준으로 출력
-								&D3DXVECTOR3(fX, fY, 0.f), // 출력할 이미지 중심 축에 대한 vec3 구조체 포인터, null인 경우 0, 0이 중심 좌표
-								nullptr, // 위치 좌표에 대한 vec3 구조체 포인터, null인 경우 스크린 상 0,0 좌표에 출력
-								D3DCOLOR_ARGB(255, 255, 255, 255)); //출력할 원본 이미지와 섞을 색상 값, 출력 시 섞은 색상이 반영, 0xffffffff를 넘겨주면 원본 색상 유지된 상태로 출력
-
-	m_pDevice->Render_End();
 
 }
 
